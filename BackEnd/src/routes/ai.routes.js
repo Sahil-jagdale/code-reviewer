20cc4446abdf4725b7fddd747c5ca24ce379b9c1
Sahil Jagdale <sahiljagdale@Sahils-Laptop.local>
2025-03-20 02:41:25 +0530

    first commit

---
+++ BackEnd/src/routes/ai.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const aiControllers = require('../controllers/ai.controller')
+
+const router = express.Router();
+
+router.post("/get-review", aiControllers.getReview);
+
+
+module.exports = router;
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import Markdown from "react-markdown"
+import axios from "axios"
+import prism from "prismjs"
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState(`
+                  function sum(){
+                    return 1+1;
+                  }
+                `)
+  useEffect(() => {
+    prism.highlightAll();
+  })
+  const [review, setReview] = useState(``)
+  async function reviewCode() {
+    const response = await axios.post("http://localhost:3000/ai/get-review", { code });
+    setReview(response.data);
+  }
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 15,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }
+              }
+            />
+          </div>
+          <div className="review" onClick={reviewCode}>Review</div>
+        </div>
+        <div className="right">
+          <Markdown>{review}</Markdown>
+        </div>
+      </main >
+    </>
+  )
+}
+
+export default App
